26cfec7f64efe6bc15a52a61973d2d24538dec50
Tkhorik Dmitry <marvelmd@gmail.com>
2018-07-11 23:10:09 +0300

    изучение аннотации @Profile и default @Profile

--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.services;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Component
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
@@ -14,4 +16,15 @@ public class GreetingServiceImpl implements GreetingService {
     public String sayGreeting() {
         return HELLO_GURUS;
     }
+
+    public String getEnglishGreating(){
+        return "Hello in English!";
+    }
+    public String getGermanGreating(){
+        return "Hello in German!";
+    }
+    public String getSpanishGreating(){
+        return "Hello in Spanosh!";
+    }
+
 }
